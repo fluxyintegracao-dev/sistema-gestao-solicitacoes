49c318299947bb5bb8b620cdef065023e7df638e
jrvjunior93-dev <jrvjunior93@gmail.com>
2026-06-25 15:33:53 -0300

    fix: evita tabela temporaria na migracao de aprovacao

--- backend/migrations/202606250001_payment_batch_single_approval.js
+++ backend/migrations/202606250001_payment_batch_single_approval.js
@@ -1,27 +1,22 @@
 module.exports = {
   async up({ sequelize }) {
-    await sequelize.query('DROP TEMPORARY TABLE IF EXISTS tmp_payment_batches_single_approval');
-
-    await sequelize.query(`
-      CREATE TEMPORARY TABLE tmp_payment_batches_single_approval AS
-      SELECT
-        b.id AS payment_batch_id,
-        MIN(a.aprovado_em) AS aprovado_em,
-        MIN(a.aprovado_por) AS aprovado_por
-      FROM payment_batches b
-      INNER JOIN payment_approvals a
-        ON a.entity_type = 'BATCH'
-       AND a.entity_id = b.id
-       AND a.acao = 'APPROVE'
-       AND a.status = 'APROVADO'
-       AND a.snapshot_hash IS NOT NULL
-      WHERE b.status = 'PENDENTE_APROVACAO'
-      GROUP BY b.id
-    `);
-
     await sequelize.query(`
       UPDATE payment_batches b
-      INNER JOIN tmp_payment_batches_single_approval tmp
+      INNER JOIN (
+        SELECT
+          batch_candidates.id AS payment_batch_id,
+          MIN(a.aprovado_em) AS aprovado_em,
+          MIN(a.aprovado_por) AS aprovado_por
+        FROM payment_batches batch_candidates
+        INNER JOIN payment_approvals a
+          ON a.entity_type = 'BATCH'
+         AND a.entity_id = batch_candidates.id
+         AND a.acao = 'APPROVE'
+         AND a.status = 'APROVADO'
+         AND a.snapshot_hash IS NOT NULL
+        WHERE batch_candidates.status = 'PENDENTE_APROVACAO'
+        GROUP BY batch_candidates.id
+      ) tmp
         ON tmp.payment_batch_id = b.id
       SET b.status = 'APROVADO',
           b.aprovacao_status = 'APROVADO',
@@ -31,7 +26,18 @@ module.exports = {
 
     await sequelize.query(`
       UPDATE payment_batch_items i
-      INNER JOIN tmp_payment_batches_single_approval tmp
+      INNER JOIN (
+        SELECT batch_candidates.id AS payment_batch_id
+        FROM payment_batches batch_candidates
+        INNER JOIN payment_approvals a
+          ON a.entity_type = 'BATCH'
+         AND a.entity_id = batch_candidates.id
+         AND a.acao = 'APPROVE'
+         AND a.status = 'APROVADO'
+         AND a.snapshot_hash IS NOT NULL
+        WHERE batch_candidates.status = 'APROVADO'
+        GROUP BY batch_candidates.id
+      ) tmp
         ON tmp.payment_batch_id = i.payment_batch_id
       SET i.status = 'APROVADO',
           i.updatedAt = NOW()
@@ -42,7 +48,21 @@ module.exports = {
       UPDATE payment_intents pi
       INNER JOIN payment_batch_items i
         ON i.payment_intent_id = pi.id
-      INNER JOIN tmp_payment_batches_single_approval tmp
+      INNER JOIN (
+        SELECT
+          batch_candidates.id AS payment_batch_id,
+          MIN(a.aprovado_em) AS aprovado_em,
+          MIN(a.aprovado_por) AS aprovado_por
+        FROM payment_batches batch_candidates
+        INNER JOIN payment_approvals a
+          ON a.entity_type = 'BATCH'
+         AND a.entity_id = batch_candidates.id
+         AND a.acao = 'APPROVE'
+         AND a.status = 'APROVADO'
+         AND a.snapshot_hash IS NOT NULL
+        WHERE batch_candidates.status = 'APROVADO'
+        GROUP BY batch_candidates.id
+      ) tmp
         ON tmp.payment_batch_id = i.payment_batch_id
       SET pi.status = 'APROVADO',
           pi.aprovado_em = COALESCE(pi.aprovado_em, tmp.aprovado_em, NOW()),
@@ -50,7 +70,5 @@ module.exports = {
           pi.updatedAt = NOW()
       WHERE pi.status = 'PENDENTE_APROVACAO'
     `);
-
-    await sequelize.query('DROP TEMPORARY TABLE IF EXISTS tmp_payment_batches_single_approval');
   }
 };
